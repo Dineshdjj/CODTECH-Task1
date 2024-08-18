9bfdce6baa1a299610885d5806907208db35f927
DINESH.R <94304959+Dineshdjj@users.noreply.github.com>
2024-08-19 00:13:24 +0530

    ..

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
@@ -11,10 +12,13 @@ const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
+
+
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
@@ -134,5 +138,6 @@ app.get('/post/:id', async (req, res) => {
   res.json(postDoc);
 })
 
-app.listen(4000);
-//
+app.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000');
+});
--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,6 +1,8 @@
 import Post from "../Post";
 import {useEffect, useState} from "react";
 
+
+
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
